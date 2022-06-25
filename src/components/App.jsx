487689a505b1a9c35610d90117edbc53cccf347a
Idris <iblaev.idris@mail.ru>
2022-06-25 15:41:42 +0300

    сделаны и проверены карточка  таскайтем и вывод по категорий

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,7 @@ import CategoriesPage from "./pages/categoriesPage";
 import AllTasks from "./pages/categoriesPage/AllTasks";
 import TasksOnCategories from "./pages/categoriesPage/TasksOnCategories";
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/categories" element={<CategoriesPage />}>
-          <Route index element={<AllTasks />} />
-          <Route path="/categories/:id" element={<TasksOnCategories />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <div></div>
 };
 
 export default App;
