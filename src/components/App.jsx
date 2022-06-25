5296ca00d8cc810d9c34d44bf5c5ee5e1b4b7e36
Idris <iblaev.idris@mail.ru>
2022-06-25 15:42:09 +0300

    сделаны и проверены карточка  таскайтем и вывод по категорий

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router";
+
 import "./App.css";
-import CategoriesPage from "./pages/categoriesPage";
-import AllTasks from "./pages/categoriesPage/AllTasks";
-import TasksOnCategories from "./pages/categoriesPage/TasksOnCategories";
+
 const App = () => {
   return <div></div>
 };
