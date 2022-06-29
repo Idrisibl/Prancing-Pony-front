f8fe0d9016ed96f005e6f9582594a82e61f0a9d2
«Mansur» <sozd.space@gmail.com>
2022-06-29 15:19:54 +0300

    закончил пагинацию

--- src/components/pages/categoriesPage/AllTasks.jsx
+++ src/components/pages/categoriesPage/AllTasks.jsx
@@ -3,8 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchTasks } from "../../../features/tasksSlice";
 import TasksItems from "../../TasksItems";
 import styles from "./Categories.module.css";
+import { useState } from "react";
 
 const AllTasks = () => {
+  const [counter, setCounter] = useState(0);
+  const [visible, setVisible] = useState(9);
+
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 9);
+    setCounter(counter + 1);
+  };
+
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasksSlice.tasks);
 
@@ -14,9 +23,16 @@ const AllTasks = () => {
 
   return (
     <div className={styles.tasks}>
-      {tasks.map((item) => {
-        return <TasksItems key={item._id} task={item} />;
-      })}
+      {tasks.slice(0, visible).map((item) => (
+        <TasksItems key={item._id} task={item} />
+      ))}{" "}
+      <button
+        className={styles.btnShowMore}
+        disabled={tasks.length === tasks.slice(0, visible).length ? true : null}
+        onClick={showMoreItems}
+      >
+        Показать еще
+      </button>
     </div>
   );
 };
--- src/components/pages/categoriesPage/TasksOnCategories.jsx
+++ src/components/pages/categoriesPage/TasksOnCategories.jsx
@@ -3,10 +3,18 @@ import { useParams } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCategoriesTasks } from "../../../features/tasksSlice";
 import TasksItems from "../../TasksItems";
-import styles from './Categories.module.css'
+import styles from "./Categories.module.css";
+import { useState } from "react";
 
 const TasksOnCategories = () => {
   const { id } = useParams();
+  const [counter, setCounter] = useState(0); 
+  const [visible, setVisible] = useState(9);
+
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 9);
+    setCounter(counter + 1);
+  };
 
   const tasks = useSelector((state) => state.tasksSlice.tasks);
   const dispatch = useDispatch();
@@ -17,9 +25,16 @@ const TasksOnCategories = () => {
 
   return (
     <div className={styles.tasks}>
-      {tasks.map((item) => {
-        return <TasksItems key={item._id} task={item} />;
-      })} 
+      {tasks.slice(0, visible).map((item) => (
+        <TasksItems key={item._id} task={item} />
+      ))}{" "}
+      <button
+        className={styles.btnShowMore}
+        disabled={tasks.length === tasks.slice(0, visible).length ? true : null}
+        onClick={showMoreItems}
+      >
+        Показать еще
+      </button>
     </div>
   );
 };
