b6669b87e52496eed8aa3df363d489d4e3ed756e
Idris <iblaev.idris@mail.ru>
2022-06-30 18:18:35 +0300

    cделал сортировку по юзера по рейтингу

--- src/components/pages/AllUsersPage/index.jsx
+++ src/components/pages/AllUsersPage/index.jsx
@@ -2,19 +2,27 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllUsers } from "../../../features/authSlice";
 import UserCard from "../../UserCard";
-import { alfMaxSort, alfMinSort } from "../../../features/authSlice";
+import { alfMaxSort, alfMinSort, ratingMaxSort, ratingMinSort } from "../../../features/authSlice";
 
 const AllUsersPage = () => {
   const users = useSelector((state) => state.auth.users);
   const dispatch = useDispatch();
 
 
-  const sortMax = () => {
+  const nameSortMax = () => {
     dispatch(alfMaxSort());
   };
 
-  const sortMin = () => {
+  const nameSortMin = () => {
     dispatch(alfMinSort());
+  };
+
+  const ratingSortMax = () => {
+    dispatch(ratingMaxSort());
+  };
+
+  const ratingSortMin = () => {
+    dispatch(ratingMinSort());
   };
 
   useEffect(() => {
@@ -26,11 +34,20 @@ const AllUsersPage = () => {
       <h1>Пользователи</h1>
       <div>
           Упорядочить по алфавиту
-          <button class="material-symbols-outlined" onClick={sortMax}>
-            +
+          <button class="material-symbols-outlined" onClick={nameSortMax}>
+            А-Я
           </button>
-          <button class="material-symbols-outlined" onClick={sortMin}>
-            -
+          <button class="material-symbols-outlined" onClick={nameSortMin}>
+            Я-А
+          </button>
+        </div>
+        <div>
+          Упорядочить по рейтингу
+          <button class="material-symbols-outlined" onClick={ratingSortMax}>
+            Сначала по топовым
+          </button>
+          <button class="material-symbols-outlined" onClick={ratingSortMin}>
+            Сначала по низким
           </button>
         </div>
       {users.map((user) => (
